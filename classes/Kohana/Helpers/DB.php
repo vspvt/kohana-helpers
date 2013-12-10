11faac30b1b4d062f292c0adecea7e4f75572f42
Vad Skakov <vad.skakov@gmail.com>
2013-12-10 20:15:14 +0400

    0.1.7

--- classes/Kohana/Helpers/DB.php
+++ classes/Kohana/Helpers/DB.php
@@ -5,10 +5,22 @@
 
 class Kohana_Helpers_DB
 {
+	protected static $transactions = [];
+
+	protected static function instanceName($instance = NULL)
+	{
+		return NULL === $instance ? Database::$default : $instance;
+	}
+
 	protected static function _instance($instance = NULL)
 	{
 		if (!class_exists('Kohana_Database')) {
 			throw new Exception('Kohana_Database module not loaded');
+		}
+		$instance = self::instanceName($instance);
+
+		if (!array_key_exists($instance, static::$transactions)) {
+			static::$transactions[$instance] = FALSE;
 		}
 
 		return Kohana_Database::instance($instance);
@@ -22,7 +34,13 @@ class Kohana_Helpers_DB
 	 */
 	static function begin($instance = NULL, $mode = NULL)
 	{
-		return self::_instance($instance)->begin($mode);
+		$object = self::_instance($instance);
+		$instance = self::instanceName($instance);
+
+		if (static::$transactions[$instance]) return FALSE;
+		static::$transactions[$instance] = $object->begin($mode);
+
+		return static::$transactions[$instance];
 	}
 
 	/**
